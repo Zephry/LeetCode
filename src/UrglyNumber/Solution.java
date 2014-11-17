aefd31b300be95f6261110ea39910966b3abde2d
Zephry <454410517@qq.com>
2014-11-18 00:37:01 +0800

    Update

--- src/UrglyNumber/Solution.java
+++ src/UrglyNumber/Solution.java
@@ -11,22 +11,24 @@ public class Solution {
 		num[0] = 2;
 		num[1] = 3;
 		num[2] = 5;
-		for(int i=0;i<845;i++) {
-			System.out.println(findNumNom(num, i));
+		for(int i=1;i<600;i++) {
+//			System.out.println(findNumNom(num, i));
+			System.out.println(fastFindNumNom(num,i));
 		}
+			//2460375
 	}
 	
 	
-	public static int findNumNom(int[] num, int m) {
+	public static long findNumNom(int[] num, int m) {
 		int len = num.length;
-		if(len == 0) {
+		if(len == 0 || m == 0) {
 			return 0;
 		}
 		int count = 0;
-		int currentNum = 2;
+		long currentNum = 2;
 		
 		while(count != m) {
-			int tmpNum = currentNum;
+			long tmpNum = currentNum;
 			for(int i=0;i<len;i++) {
 				while(tmpNum % num[i] == 0) {
 					tmpNum = tmpNum / num[i];
@@ -42,19 +44,57 @@ public class Solution {
 	}
 	
 	
-	public static int fastFindNumNom(int[] num, int m) {
+	public static long fastFindNumNom(int[] num, int m) {
 		int len = num.length;
-		if(len == 0) {
+		if(len == 0 || m == 0) {
 			return 0;
 		}
 		int count = 0;
-		List<Integer> uglyNum = new ArrayList<Integer>();
-		uglyNum.add(1);
-		int max = 1;
-		while((uglyNum.size()-1) != m) {
-			
-		}
+		long[] uglyNum = new long[10000];
+		uglyNum[count] = 1;
 		
+		
+
+		count++;
+		uglyNum[count] = num[0];
+			
+
+		long max = uglyNum[count];
+		
+		while(count != m) {
+			long[] maxCandidates = new long[len];			//被乘数
+			long[] minCandidates = new long[len];			//乘上之后的结果，用来找出最小的
+			
+			for(int n=0;n<len;n++) {
+				for(int i=count;i>=0;i--) {
+					if(i == 0 && num[n]*uglyNum[i] > max) {
+						maxCandidates[n] = i;
+						minCandidates[n] = num[n]*uglyNum[i];
+						break;
+					}else {
+						if(num[n]*uglyNum[i-1] <= max && num[n]*uglyNum[i] > max) {
+							maxCandidates[n] = i;
+							minCandidates[n] = num[n]*uglyNum[i];
+							break;
+						}
+					}
+				}
+			}
+			count++;
+			uglyNum[count] = findMin(minCandidates);
+			max = uglyNum[count];
+		}
+		return uglyNum[count];
 	}
+	private static long findMin(long[] minCandidates) {
+		long min = Long.MAX_VALUE;
+		for(long thisMin : minCandidates) {
+			if(thisMin < min && thisMin != 0) {
+				min = thisMin;
+			}
+		}
+		return min;
+	}
+	
 	
 }
