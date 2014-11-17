2d5fc5ec0e1edc0344badbfa33e63e79c813ec8d
i302649 <zephyr.du@sap.com>
2014-11-17 17:29:02 +0800

    Answer

---
+++ src/UrglyNumber/Solution.java
@@ -0,0 +1,41 @@
+package UrglyNumber;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] num = new int[3];
+		num[0] = 2;
+		num[1] = 3;
+		num[2] = 5;
+		for(int i=0;i<845;i++) {
+			System.out.println(findNumNom(num, i));
+		}
+	}
+	
+	
+	public static int findNumNom(int[] num, int m) {
+		int len = num.length;
+		if(len == 0) {
+			return 0;
+		}
+		int count = 0;
+		int currentNum = 2;
+		
+		while(count != m) {
+			int tmpNum = currentNum;
+			for(int i=0;i<len;i++) {
+				while(tmpNum % num[i] == 0) {
+					tmpNum = tmpNum / num[i];
+				}
+			}
+			if(tmpNum == 1) {
+				count++;
+			}
+			currentNum++;
+		}
+		
+		return currentNum-1;
+	}
+
+}
