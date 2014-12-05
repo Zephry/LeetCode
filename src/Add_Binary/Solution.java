fe9d7f7579114eb538a70ed7569bb3fb788122c5
i302649 <zephyr.du@sap.com>
2014-12-05 18:03:58 +0800

    Add binary

---
+++ src/Add_Binary/Solution.java
@@ -0,0 +1,38 @@
+package Add_Binary;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public String addBinary(String a, String b) {
+    	Double aNum = Double.valueOf(a);
+    	Double bNum = Double.valueOf(b);
+    	
+    	aNum
+    	
+    	
+    	
+    }
+    
+    public double changeToTen(double binary) {
+    	int len = 0;
+    	double tmp = binary;
+    	
+    	while(binary > Math.pow(10, len)) {
+    		len++;
+    	}
+    	
+    	
+    	double result = 0;
+    	
+    	for(int i=0;i<len;i++) {
+    		
+    	}
+    	
+    	
+    }
+
+}
