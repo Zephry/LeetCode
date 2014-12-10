e564986172a3537e4f82644341d1f2ff3019c641
i302649 <zephyr.du@sap.com>
2014-12-10 17:20:07 +0800

    Triangle

--- src/Triangle/Solution.java
+++ src/Triangle/Solution.java
@@ -6,10 +6,31 @@ public class Solution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 	public int minimumTotal(List<List<Integer>> triangle) {
-        return 0;
+        int row = triangle.size();
+        if(row == 0) {
+        	return 0;
+        }
+        int[] d = new int[row];
+        for(int i=0;i<row;i++) {
+        	for(int j=0;j<row-i;j++) {
+        		if(i==0) {
+        			d[j] = triangle.get(row-1).get(j);
+        		}else {
+        			d[j] = triangle.get(row-1-i).get(j) + min(d[j],d[j+1]);
+        		}
+        	}
+        }
+        return d[0];
     }
-
+	
+	public int min(int a, int b) {
+		if(a<b) {
+			return a;
+		}else {
+			return b;
+		}
+	}
 }
