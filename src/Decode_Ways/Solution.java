fc4a36f8a1be3c64b66b7821ee4469164976e954
i302649 <zephyr.du@sap.com>
2014-12-19 13:31:01 +0800

    update

--- src/Decode_Ways/Solution.java
+++ src/Decode_Ways/Solution.java
@@ -4,34 +4,44 @@ public class Solution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		numDecodings("27");
 	}
 	
-    public int numDecodings(String s) {
+    public static int numDecodings(String s) {
     	String[] tmp = s.split("");
     	String[] nums = new String[tmp.length-1];
     	for(int i=1;i<tmp.length;i++) {
     		nums[i-1] = tmp[i];
     	}
     	int len = nums.length;
-    	int last = -1;
+    	if(len == 0) {
+    		return 0;
+    	}
     	int now = -1;
-    	int[] count = new int[len];
-    	
-    	for(int i=0;i<len;i++) {
+    	int[] count = new int[2];
+    	count[0] = 1;
+    	if(nums[0].equals("0")) {
+    		return 0;
+    	}
+    	count[1] = 1;
+    	for(int i=1;i<len;i++) {
     		now = Integer.valueOf(nums[i]);
-    		if(last == -1) {
-    			count[i] = 1;
-    		}else {
-    			int t = (last * 10) + now;
-    			if(t>26) {
-    				count[i] = count[i-1];
-    			}else {
-    				count[i] = count[i-1] + count[i-2];//TODO
-    			}
+    		if(now == 0) {
+    			count[1] = 0;
     		}
+			int t = (count[1] * 10) + now;
+			if(t>26) {
+				count[0] = count[1];
+				
+			}else {
+				int tmpCount = count[1];
+				count[1] = count[0] + count[1];
+				count[0] = tmpCount;
+			}
+    		
     		
     	}
+    	return count[1];
     	
     	
     	
