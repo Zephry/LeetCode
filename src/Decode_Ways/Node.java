328794d92e01438017c92f61f38cd8d7fca6de71
i302649 <zephyr.du@sap.com>
2015-01-07 15:56:21 +0800

    Update

--- src/Decode_Ways/Node.java
+++
@@ -1,9 +0,0 @@
-package Decode_Ways;
-
-public class Node {
-	int val;
-	
-	public Node(int x) {
-		val = x;
-	}
-}
--- src/Decode_Ways/Solution.java
+++ src/Decode_Ways/Solution.java
@@ -1,71 +1,64 @@
 package Decode_Ways;
 
-import java.lang.reflect.Field;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Solution {
 
-	public static void main(String[] args) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		numDecodings("27");
-		int[] array = {1,2};
-		change((array[0]), array[1]);
-		System.out.println(array[0]);
+		System.out.println(numDecodings("100"));
+		
+
 	}
 	
     public static int numDecodings(String s) {
-    	String[] tmp = s.split("");
-    	String[] nums = new String[tmp.length-1];
-    	for(int i=1;i<tmp.length;i++) {
-    		nums[i-1] = tmp[i];
-    	}
-    	int len = nums.length;
+
+    	int len = s.length();
     	if(len == 0) {
     		return 0;
     	}
-    	int now = -1;
-    	int[] count = new int[2];
-    	count[0] = 1;
-    	if(nums[0].equals("0")) {
+    	String[] tmp = s.split("");
+    	int[] nums  = new int[len];
+    	
+    	for(int i=1;i<len+1;i++) {
+    		nums[i-1] = Integer.valueOf(tmp[i]);
+    	}
+    	
+    	if(nums[0] == 0) {
     		return 0;
     	}
+    	
+    	int[] count = new int[len+1];
+    	count[0] = 1;
     	count[1] = 1;
-    	for(int i=1;i<len;i++) {
-    		now = Integer.valueOf(nums[i]);
-    		if(now == 0) {
-    			count[1] = 0;
+    	
+    	for(int i=2;i<len+1;i++) {
+    		if(nums[i-1] == 0) {
+    			if(nums[i-2] == 0) {
+    				return 0;
+    			}
+    			if((nums[i-2]*10)+nums[i-1] > 26) {
+    				return 0;
+    			}else {
+    				count[i] = count[i-2];
+    				
+    			}
+    		}else {
+    			if(nums[i-2] == 0) {
+    				count[i] = count[i-1];
+    			}else {
+	    			if((nums[i-2]*10)+nums[i-1] > 26) {
+	    				count[i] = count[i-1];
+	    			}else {
+	    				count[i] = count[i-1] + count[i-2];
+	    			}
+    			}
     		}
-			int t = (count[1] * 10) + now;
-			if(t>26) {
-				count[0] = count[1];
-				
-			}else {
-				int tmpCount = count[1];
-				count[1] = count[0] + count[1];
-				count[0] = tmpCount;
-			}
-    		
-    		
     	}
-    	return count[1];
     	
     	
-    	
-    	
+    	return count[len];
     }
     
-    public static void change(Integer a, Integer b) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-    	Field f = a.getClass().getDeclaredField("value");
-    	f.setAccessible(true);           
-    	f.setInt(a, 2);
-//    	f = b.getClass().getDeclaredField("value");
-//    	f.setAccessible(true);           
-//    	f.setInt(b, b);
-//    	Node tmp = a;
-//    	a.val = 2;
-//    	a.val = b.val;
-//    	b = tmp;
-//    	a = 2;
-    }
+    
 
 }
