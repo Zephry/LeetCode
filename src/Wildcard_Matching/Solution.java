2fbf9e7c2a57c28f432475f7470415d64802be89
i302649 <zephyr.du@sap.com>
2015-01-19 18:12:22 +0800

    Update

--- src/Wildcard_Matching/Solution.java
+++ src/Wildcard_Matching/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(isMatch("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "*aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+		System.out.println(isMatch("abcderfgislghtnxl", "a*c***********************************************************f*isl*gh?nxl"));
 	}
 	
 	public static boolean isMatch(String s, String p) {
@@ -16,86 +16,64 @@ public class Solution {
 			return true;
 		}else if(pLen == 0) {
 			return false;
-		}
-		
-		String pattern = String.valueOf(p.charAt(0));
-		
-		for(int i=1;i<pLen;i++) {
-			if(p.charAt(i-1) == p.charAt(i) && p.charAt(i) == '*') {
-				
-			}else {
-				pattern = pattern + String.valueOf(p.charAt(i));
-			}
-		}
-		
-		return Match(s, pattern, 0, 0, sLen, pattern.length());
-		
-    }
-
-	public static boolean Match(String s, String p, int sPosition, int pPosition, int sLen, int pLen) {
-		
-		while(sPosition < sLen && pPosition < pLen) {
-			Character letter = p.charAt(pPosition);
-			Character target = s.charAt(sPosition);
-			if(letter.equals('*')) {
-				for(int i=sPosition;i<sLen;i++) {
-					if(Match(s, p, i, pPosition+1, sLen, pLen)) {
-						return true;
-					}
-				}
-				return false;
-			}else if(letter.equals('?')) {
-				
-			}else {
-				if(!letter.equals(target)) {
-					return false;
-				}
-				
-			}
-			
-			sPosition++;
-			pPosition++;
-			
-			
-		}
-		if(sPosition == sLen && (pPosition-pLen) == 1 && p.charAt(pPosition) == '*') {
+		}else if(sLen == 0 && p.equals("*")) {
 			return true;
-		}else if(sPosition == sLen && pPosition == pLen) {
-			return true;
+		}else if(sLen == 0) {
+			return false;
+		}
+		
+		
+		boolean[] last = new boolean[sLen];
+		boolean[] next = new boolean[sLen];
+		
+		if(s.charAt(0) == p.charAt(0) || p.charAt(0) == '?' || p.charAt(0) == '*') {
+			last[0] = true;
 		}else {
 			return false;
 		}
 		
-	}
+		for(int j = 0;j<pLen;j++) {
+			for(int i = 0;i<sLen;i++) {
+			
+				if(last[i]) {
+					if(p.charAt(j) == '*') {
+						if(i<sLen-1) {
+							last[i+1] = true;
+						}
+					}
+					if(j<pLen-1 && i<sLen-1) {
+						if(s.charAt(i+1) == p.charAt(j+1) || p.charAt(j+1) == '?') {
+							next[i+1] = true;
+						}
+					}
+					if(j<pLen-1) {
+						if(p.charAt(j+1) == '*') {
+							next[i] = true;
+							
+						}
+					}
+										
+				}
+			}
+			if(j<pLen-1) {
+				last = next;
+				next = new boolean[sLen];
+			}
+		}
+		
+		return last[sLen-1];
+		
+		
+    }
+
+	
+	
+	
+	
+	
 }
 
 
-//Character letter = p.charAt(pPosition);
-//if(letter.equals('?')) {
-//	if(sLen == 0) {
-//		return true;
-//	}
-//	for(int i=1;i<sLen;i++) {
-//		if(Match(s.substring(i), p.substring(1))) {
-//			return true;
-//		}
-//	}
-//	return false;
-//}else if(letter.equals('*')) {
-//	if(sLen == 0) {
-//		return false;
-//	}
-//	return Match(s.substring(1), p.substring(1));
-//}else {
-//	if(sLen == 0) {
-//		return false;
-//	}
-//	if(letter.equals(s.charAt(0))) {
-//		return Match(s.substring(1), p.substring(1));
-//	}else {
-//		return false;
-//	}
-//}
 
 
 	
